65a98c1a4aff3465ebbeab2246ddeb3befc3fc8f
Machado Rafael <rafaelmachadoignacio@gmail.com>
2022-08-13 18:25:31 -0300

    a

--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -7,10 +7,14 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+projectsList:any;
 
   constructor(private datosData:DataService) { }
 
   ngOnInit(): void {
+    this.datosData.obtenerDatos().subscribe(data => {
+      this.projectsList=data.projects;
+    })
   }
 
 }
