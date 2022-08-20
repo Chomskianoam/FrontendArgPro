ac7d150cc480334e65ad46805b0b9ed0097fd178
Machado Rafael <rafaelmachadoignacio@gmail.com>
2022-08-19 22:19:31 -0300

    Se crearon los métodos para que al iniciar sesión cambie el botón, al igual que la posibilidad de salir del modo y borrar el token

--- frontend/src/app/components/btn-log-in/btn-log-in.component.ts
+++ frontend/src/app/components/btn-log-in/btn-log-in.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-btn-log-in',
@@ -7,14 +8,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./btn-log-in.component.css']
 })
 export class BtnLogInComponent implements OnInit {
-
-  constructor(private Router:Router) { }
+  isLogged = false;
+  constructor(private Router:Router, private tokenService: TokenService) { }
 
   ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    }else{
+      this.isLogged = false;
+    }
   }
 
   login(){
     this.Router.navigate(['/login']);
+  }
+
+  onLogOut(): void{
+    this.tokenService.logOut();
+    window.location.reload();
   }
 
 
--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from 'src/app/services/token.service';
 
 
 @Component({
@@ -8,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    
   }
+
+
 
 }
